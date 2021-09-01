eceaf4c0294934075e78331f18725b5145a291ff
ArabAmerican <85521717+ArabAmerican@users.noreply.github.com>
2021-09-01 15:49:39 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 echo "Attempting to install necessary components"
 sudo chmod +x ydns.sh
-sudo apt-get update
+sudo apt-get update --allow-releaseinfo-change #This prevents issues with Raspbian
 sudo apt-get --yes install curl
 sudo apt-get --yes install dnsutils
 echo "Done."
